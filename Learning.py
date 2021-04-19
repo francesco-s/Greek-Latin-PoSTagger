0743fdb9c29ea01103e02d0a83f2de2231969b9f
francesco-s <61080516+francesco-s@users.noreply.github.com>
2021-04-19 19:43:05 +0200

    Update structures

--- Learning.py
+++ Learning.py
@@ -9,46 +9,64 @@ Created on Thu Apr 15 15:00:17 2021
 import csv
 from collections import Counter
 import math
+import numpy as np
+from array import array
+from collections import defaultdict
 
 #  import pandas as pd
 #data = pd.read_csv("./Bank/Latino/la_llct-ud-test.conllu", nrows=1)
 
 w_t = []
+w_t.append(('INIT', 'INIT'))
 
-with open("./Bank/Latin/la_llct-ud-test.conllu") as fd:
+with open("./Bank/Latin/la_llct-ud-dev.conllu") as fd:
     rd = csv.reader(fd, delimiter="\t", quotechar='"')
     for row in rd:
         if ((len(row))> 3):
             w_t.append((row[1], row[3]))
         if (len(row) == 0):
-            w_t.append(('---', '---'))
+            w_t.append(('END', 'END'))
+            w_t.append(('INIT', 'INIT'))
+    w_t.pop()
+            
             
             
 w_t_occ=Counter(w_t)
 t_occ=Counter([i[1] for i in w_t])
 
-p_emission = []
-p_transition = []
+p_emission = dict()
+p_transition = dict()
+
 
 #compute emission probability
+#prob w given t
 for key, value in w_t_occ.items():
-    wt_c =t_occ.get(key[1])
-    p_emission.append((key[0], key[1], math.log(value/wt_c)))
-
-
+    if key[1] in p_emission:
+        p_emission[key[1]].append([key[0], value/t_occ.get(key[1])])
+    else:
+        p_emission[key[1]] = [[key[0], value/t_occ.get(key[1])]]
+    
 #compute transition probability
+#prob t1 given t
 for t1 in t_occ.keys():
-    if t1 != '---':
-        for t in t_occ.keys():
-            if t != t1 and t != '---':
-                count = 0
-                for i in range(1, len(w_t)):
-                    if w_t[i][1] == t1:
-                        if w_t[i-1][1] == t:
-                            count += 1
-                p_transition.append((t1, t, count/t_occ.get(t)))
+    for t in t_occ.keys():
+        count = 0
+        for i in range(1, len(w_t)):
+            if w_t[i][1] == t1:
+                if w_t[i-1][1] == t:
+                    count += 1
+        if t in p_transition:
+            p_transition[t].append([t1, count/t_occ.get(t)])
+        else:
+            p_transition[t] = [[t1, count/t_occ.get(t)]]
+            
+            
+y='quinta'
+Pi=None
 
-                            
-                            
-                    
-                
+
+array_p_transition = np.array(list(p_transition.items()))
+array_p_emission = np.array(list(p_emission.items()))
+
+
+
