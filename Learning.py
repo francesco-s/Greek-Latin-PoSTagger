411c21774dfa5a8104456d8551836dcdc928bd0c
francesco-s <61080516+francesco-s@users.noreply.github.com>
2021-04-22 00:17:20 +0200

    viterbi beta

--- Learning.py
+++ Learning.py
@@ -8,65 +8,128 @@ Created on Thu Apr 15 15:00:17 2021
 
 import csv
 from collections import Counter
-import math
+#import math
 import numpy as np
-from array import array
-from collections import defaultdict
+#from array import array
+#from collections import defaultdict
 
 #  import pandas as pd
-#data = pd.read_csv("./Bank/Latino/la_llct-ud-test.conllu", nrows=1)
+# data = pd.read_csv("./Bank/Latino/la_llct-ud-test.conllu", nrows=1)
 
 w_t = []
+w_s= []
 w_t.append(('INIT', 'INIT'))
 
 with open("./Bank/Latin/la_llct-ud-dev.conllu") as fd:
     rd = csv.reader(fd, delimiter="\t", quotechar='"')
+    i = -1
     for row in rd:
-        if ((len(row))> 3):
+        if len(row) > 3:
+            if i == 0:
+                w_s.append((row[1], 'INIT'))
+                i = 1
             w_t.append((row[1], row[3]))
-        if (len(row) == 0):
-            w_t.append(('END', 'END'))
+        if len(row) == 0:
             w_t.append(('INIT', 'INIT'))
+            i = 0
     w_t.pop()
-            
-            
-            
-w_t_occ=Counter(w_t)
-t_occ=Counter([i[1] for i in w_t])
+
+w_t_occ = Counter(w_t)
+w_s_occ = Counter(w_s)
+
+t_occ = Counter([i[1] for i in w_t])
 
 p_emission = dict()
+p_emission_init = dict()
 p_transition = dict()
 
-
-#compute emission probability
-#prob w given t
+# compute emission probability
+# prob w given t
 for key, value in w_t_occ.items():
-    if key[1] in p_emission:
-        p_emission[key[1]].append([key[0], value/t_occ.get(key[1])])
+    prob = value / t_occ.get(key[1])
+    '''if key[1] in p_emission:
+        p_emission[key[1]].append([key[0], prob])
     else:
-        p_emission[key[1]] = [[key[0], value/t_occ.get(key[1])]]
-    
-#compute transition probability
-#prob t1 given t
+        p_emission[key[1]] = [[key[0], prob]]'''
+    if key[1] in p_emission:
+        p_emission[key[1]].update({key[0]: prob})
+    else:
+        p_emission[key[1]] = {key[0]: prob}
+
+#compute emission probability for initial state
+for key, value in w_s_occ.items():
+    prob = value / t_occ.get(key[1])
+    if key[1] in p_emission_init:
+        p_emission_init[key[1]].update({key[0]: prob})
+    else:
+        p_emission_init[key[1]] = {key[0]: prob}
+
+
+
+p_prova = dict()
+# compute transition probability
+# prob t1 given t
 for t1 in t_occ.keys():
     for t in t_occ.keys():
         count = 0
         for i in range(1, len(w_t)):
             if w_t[i][1] == t1:
-                if w_t[i-1][1] == t:
+                if w_t[i - 1][1] == t:
                     count += 1
-        if t in p_transition:
-            p_transition[t].append([t1, count/t_occ.get(t)])
+        prob = count / t_occ.get(t)
+        '''if t in p_transition:
+            p_transition[t].append([t1, prob])
         else:
-            p_transition[t] = [[t1, count/t_occ.get(t)]]
-            
-            
-y='quinta'
-Pi=None
+            p_transition[t] = [[t1, prob]]'''
+        if t in p_prova:
+            p_prova[t].update({t1: prob})
+        else:
+            p_prova[t] = {t1: prob}
+        if t in p_transition:
+            p_transition[t].append(prob)
+        else:
+            p_transition[t] = [prob]
+
+i = 0
+array_p_transition = np.zeros((len(p_transition), len(p_transition)))
+status_order = np.empty(len(p_transition), dtype=object)
+for key, values in p_transition.items():
+    status_order[i] = str(key)
+    array_p_transition[i, :] = values
+    i += 1
+
+# VITERBI
+
+y = '+ In Dei omnipotentis nomine, regnante domno nostro Karolus divina faventem clementia imperatore augusto, anno imperii eius septimo, pridie idus augusti indictione quinta.'
+Pi = None
+
+# Cardinality of the state space
+K = array_p_transition.shape[0]
+# Initialize the priors with default (uniform dist) if not given by caller
+Pi = Pi if Pi is not None else np.full(K, 1 / K)
+input_splitted = y.split()
+T = len(input_splitted)
+viterbi = np.empty((K, T), 'd')
+backpointer = np.empty((K, T), 'B')
 
 
-array_p_transition = np.array(list(p_transition.items()))
-array_p_emission = np.array(list(p_emission.items()))
+# Initilaize the tracking tables from first observation
+try:
+    viterbi[:, 0] = np.array(p_transition['INIT']) * p_emission_init['INIT'][input_splitted[0]]
+except KeyError:
+    viterbi[:, 0] = np.array(p_transition['INIT']) * 0.001
+    print ('keyerror')
+backpointer[:, 0] = 0
 
-
-
+# Iterate throught the observations updating the tracking tables
+for i in range(1, T):
+    for s in range(0, len(status_order)):
+        try:
+            viterbi[s, i] = max(viterbi[:, i - 1]) * p_emission[status_order[s]][input_splitted[i]] * p_prova[status_order[s]][status_order[s-1]]
+            backpointer[s, i] = max(viterbi[:, i - 1]) * p_emission[status_order[s]][input_splitted[i]]
+            print(f'{p_emission[status_order[s]][input_splitted[i]]}', f'{status_order[s]}', f'{input_splitted[i]}')
+        except KeyError:
+            viterbi[s, i] = max(viterbi[:, i - 1]) * 0.001
+            print ('keyerror', f'{status_order[s]}', f'{input_splitted[i]}')
+        #viterbi[:, i] = np.max(viterbi[:, i - 1] * A.T * B[np.newaxis, :, y[i]].T, 1)
+        #backpointer[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
