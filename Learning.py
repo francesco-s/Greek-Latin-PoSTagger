bbb39d54308686637be60e572703dbade1d2e4ed
francesco-s <61080516+francesco-s@users.noreply.github.com>
2021-04-27 22:45:35 +0200

    added baseline

--- Learning.py
+++ Learning.py
@@ -9,21 +9,29 @@ Created on Thu Apr 15 15:00:17 2021
 import csv
 from collections import Counter
 import numpy as np
+import math
 import nltk
+import cltk
 
+
+from nltk import word_tokenize
+from nltk.tokenize import MWETokenizer
+
+#w_t tutte le parole con proprio tag (+1 per INIT)
 w_t = []
+# w_s tutte le parole iniziali
 w_s= []
 w_t.append(('INIT', 'INIT'))
 
-with open("./Bank/Latin/la_llct-ud-dev.conllu") as fd:
+with open("./Bank/Greek/grc_perseus-ud-train.conllu") as fd:
     rd = csv.reader(fd, delimiter="\t", quotechar='"')
     i = -1
     for row in rd:
         if len(row) > 3:
             if i == 0:
-                w_s.append((row[1], 'INIT'))
+                w_s.append((row[1].lower(), 'INIT'))
                 i = 1
-            w_t.append((row[1], row[3]))
+            w_t.append((row[1].lower(), row[3].lower()))
         if len(row) == 0:
             w_t.append(('INIT', 'INIT'))
             i = 0
@@ -36,7 +44,6 @@ t_occ = Counter([i[1] for i in w_t])
 
 p_emission = dict()
 p_emission_init = dict()
-p_transition = dict()
 
 # compute emission probability
 # prob w given t
@@ -63,27 +70,28 @@ p_transition_dict = dict()
 # compute transition probability
 # prob t1 given t
 for t1 in t_occ.keys():
-    for t in t_occ.keys():
-        count = 0
-        for i in range(1, len(w_t)):
-            if w_t[i][1] == t1:
-                if w_t[i - 1][1] == t:
-                    count += 1
-        prob = count / t_occ.get(t)
-        '''if t in p_transition:
-            p_transition[t].append([t1, prob])
-        else:
-            p_transition[t] = [[t1, prob]]
-        if t in p_transition:
-            p_transition[t].append(prob)
-        else:
-            p_transition[t] = [prob]    
-        '''
-        if t in p_transition_dict:
-            p_transition_dict[t].update({t1: prob})
-        else:
-            p_transition_dict[t] = {t1: prob}
-        
+    if t1 != 'INIT':
+        for t in t_occ.keys():
+            count = 0
+            for i in range(1, len(w_t)):
+                if w_t[i][1] == t1:
+                    if w_t[i - 1][1] == t:
+                        count += 1
+            prob = count / t_occ.get(t)
+            '''if t in p_transition:
+                p_transition[t].append([t1, prob])
+            else:
+                p_transition[t] = [[t1, prob]]
+            if t in p_transition:
+                p_transition[t].append(prob)
+            else:
+                p_transition[t] = [prob]    
+            '''
+            if t in p_transition_dict:
+                p_transition_dict[t].update({t1: prob})
+            else:
+                p_transition_dict[t] = {t1: prob}
+            
 
 i = 0
 states = np.empty(len(p_transition_dict), dtype=object)
@@ -93,59 +101,156 @@ for key in p_transition_dict.keys():
 
 # VITERBI
 
-query = '+ In Dei omnipotentis nomine, regnante domno nostro Karolus divina faventem clementia imperatore augusto, anno imperii eius septimo, pridie idus augusti indictione quinta.'
+query_list = []
+first_char_init = 9
 
-input_splitted =  nltk.word_tokenize(query) #y.split()
-T = len(input_splitted)
-    
+w_t_test = []
+
+with open("./Bank/Greek/grc_perseus-ud-test.conllu") as fd:
+    rd = csv.reader(fd, delimiter="\t", quotechar='"')
+    i = -1
+    for row in rd:
+        if len(row) == 1:
+            if row[0].startswith('# text'):
+                query_list.append(row[0][first_char_init:len(row[0])].lower())
+        elif len(row) > 3:
+            w_t_test.append((row[1].lower(), row[3].lower()))
+        
+
+
+        
 def dptable(V):
     yield " ".join(("%10d" % i) for i in range(len(V)))
     for y in V[0]:
         yield "%.7s: " % y+" ".join("%.7s" % ("%f" % v[y]) for v in V)
+
+#query = '+ In Dei omnipotentis nomine, regnante domno nostro Karolus divina faventem clementia imperatore augusto, anno imperii eius septimo, pridie idus augusti indictione quinta.'
+
+all_pos = [[]]
+all_pos.pop()
+
+token_exclude = [
+                 ('[', 'adj', ']'),
+                 ('[', 'adv', ']'),
+                 ('[', 'aux', ']'),
+                 ('[', 'cconj', ']'),
+                 ('[', 'det', ']'),
+                 ('[', 'init', ']'),
+                 ('[', 'noun', ']'),
+                 ('[', 'num', ']'),
+                 ('[', 'part', ']'),
+                 ('[', 'pron', ']'),
+                 ('[', 'propn', ']'),
+                 ('[', 'punct', ']'),
+                 ('[', 'sconj', ']'),
+                 ('[', 'verb', ']'),
+                 ('[', 'x', ']'),
+                 ('[', 'Adj', ']'),
+                 ('[', 'Adv', ']'),
+                 ('[', 'Aux', ']'),
+                 ('[', 'Cconj', ']'),
+                 ('[', 'Det', ']'),
+                 ('[', 'Init', ']'),
+                 ('[', 'Noun', ']'),
+                 ('[', 'Num', ']'),
+                 ('[', 'Part', ']'),
+                 ('[', 'Pron', ']'),
+                 ('[', 'Propn', ']'),
+                 ('[', 'Punct', ']'),
+                 ('[', 'Sconj', ']'),
+                 ('[', 'Verb', ']'),
+                 ('[', 'X', ']'),
+                 ('[', '--', ']'),
+                 ('[', 'participle', ']'), ('·')
+                 ]
+
+states = np.delete(states, 0)
+
+for query in query_list:
+    tokenizer = MWETokenizer(token_exclude)
+    input_splitted = tokenizer.tokenize(word_tokenize(query))
+    #input_splitted =  cltk.word_tokenize(query) #y.split()
+    T = len(input_splitted)
+    tag_target = 'noun'
+    for t in range(0, T):
+        max = 0
+        for key, value in w_t_occ.items():
+            if key[0]==input_splitted[t]:
+                if max < value:
+                    max = value
+                    tag_target = key[1]
+        all_pos.append([input_splitted[t], tag_target])    
+
+
+'''for query in query_list:
         
-# Tracking tables from first observation
-backtrace=[{}]
-for i in states:
-    try:
-        backtrace[0][i]=p_transition_dict['INIT'][i]*p_emission[i][input_splitted[0]]
-    except KeyError:
-        backtrace[0][i]=p_transition_dict['INIT'][i]*0.001
-        #print ('keyerror')
+    tokenizer = MWETokenizer(token_exclude)
+    input_splitted = tokenizer.tokenize(word_tokenize(query))
 
-
-for t in range(1, T):
-    backtrace.append({})
-    for y in states:
-        try:
-            (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * p_emission[y][input_splitted[t]], y0) for y0 in states)
-        except KeyError:
-            # P(unk|NOUN) =1 --> quando non conosco la parola la considero un nome
-            '''if y == 'NOUN' :
-                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 1, y0) for y0 in states)
-            else:
-                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.001, y0) for y0 in states)'''
-            # P(unk|NOUN) =0.5 and P(unk|VERB) --> quando non conosco la parola la considero o un nome o un vrerbo
-            '''if y == 'NOUN' :
-                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.5, y0) for y0 in states)
-            elif y == 'VERB':
-                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.5, y0) for y0 in states)
-            else:
-                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.001, y0) for y0 in states)'''
-            #P(unk|ti) = 1/#(PoS_TAGs)
-            (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * (1 / len(states)), y0) for y0 in states)
-            
-        backtrace[t][y] = prob
-    #for i in dptable(viterbi):
-    #   print (i)
-    opt=[]
-    for j in backtrace:
-        for x,y in j.items():
-            if j[x]==max(j.values()):
-                opt.append(x)
+    #input_splitted =  nltk.word_tokenize(query) #y.split()
+    T = len(input_splitted)
     
-p=max(backtrace[-1].values())
-print ('The PoS are\n'+''
-       .join(map(''.join, zip([x + '/' for x in input_splitted], [x + '\n' for x in opt])))
-       +'\nWith probability of %s'%p)
+    # Tracking tables from first observation
+    backtrace=[{}]
+    for i in states:
+        try:
+            backtrace[0][i]=p_transition_dict['INIT'][i]*p_emission_init['INIT'][input_splitted[0]]
+        except KeyError:
+            backtrace[0][i]=p_transition_dict['INIT'][i]*0.0000011    
+    
+    
+    for t in range(1, T):
+        backtrace.append({})
+        for y in states:
+            try:
+                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * p_emission[y][input_splitted[t]], y0) for y0 in states)
+                #(prob, state) = max((backtrace[t-1][y0], y0) for y0 in states)
+                #prova = prob * p_transition_dict[state][y] * p_emission[y][input_splitted[t]]
+            except KeyError:
+                # P(unk|NOUN) =1 --> quando non conosco la parola la considero un nome
+                if y == 'noun' :
+                    (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 1, y0) for y0 in states)
+                    #(prob, state) = max((backtrace[t-1][y0], y0) for y0 in states)
+                    #prova = prob * p_transition_dict[state][y] * 1
+                else:
+                    (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.0000001, y0) for y0 in states)
+                # P(unk|NOUN) =0.5 and P(unk|VERB) --> quando non conosco la parola la considero o un nome o un verbo
+                if y == 'noun' :
+                    (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.5, y0) for y0 in states)
+                elif y == 'verb':
+                    (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.5, y0) for y0 in states)
+                else:
+                    (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.0000001, y0) for y0 in states)
+                #P(unk|ti) = 1/#(PoS_TAGs)
+                #(prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * (1 / len(states)), y0) for y0 in states)
+                (prob, state) = max((backtrace[t-1][y0] * p_transition_dict[y0][y] * 0.0000011, y0) for y0 in states)
+            backtrace[t][y] = prob
+        #for i in dptable(viterbi):
+        #   print (i)
+        opt=[]
+        for j in backtrace:
+            for x,y in j.items():
+                if j[x]==max(j.values()):
+                    opt.append(x)
         
+    p=max(backtrace[-1].values())
+    # print ('The PoS are\n'+''
+    #       .join(map(''.join, zip([x + '/' for x in input_splitted], [x + '\n' for x in opt])))
+    #       +'\nWith probability of %s'%p)
+    for l in range(0,T):
+        all_pos.append([input_splitted[l], opt[l]])'''    
+
+right_pos = 0
+wrong_pos = 0
+n = 0
+
+for word in all_pos:
+    if (word[1] == w_t_test[n][1]):
+        right_pos +=1
+    else :
+        wrong_pos +=1
+    n += 1
+accuracy = right_pos/(right_pos+wrong_pos)
+print(accuracy)
+
         
